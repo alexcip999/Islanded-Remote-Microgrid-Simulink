b4964133daa627e121b4f8282760136ef7853a22
alexcip999 <constantinalexandru123@gmail.com>
2026-03-25 19:31:02 +0200

    initial commit

---
+++ scd3busMicrogridDroopControl_variants_enum.m
@@ -0,0 +1,10 @@
+classdef scd3busMicrogridDroopControl_variants_enum < Simulink.IntEnumType
+    % enum for scd3busMicrogridDroopControl example
+
+    % Copyright 2022 The MathWorks, Inc.
+    enumeration
+        Low (1)
+        Medium (2)
+        High (3)
+    end
+end
